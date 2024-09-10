ed8d9063f64bdcee89b5735b092391295a87b955
saviobueno <saviobueno@gmail.com>
2024-09-09 22:06:35 -0300

    fundamentos 003

---
+++ index.js
@@ -0,0 +1,15 @@
+let meta = {
+    value: 'ler um livro por mês',
+    checked: false,
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(meta.value)
+console.log(metas[1].value)
